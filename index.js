4121882e24c5f4a1f068e41c7f2470c250d2a9cf
sonvendra <sonvendra@gmail.com>
2020-06-25 21:57:52 +0530

    google athentication

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+require('./models/Users')
+require('./services/passport');
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'Welcome'});
-})
+mongoose.connect(keys.mongoURI)
+
+require('./routes/authRoutes')(app);
+
+// app.get('/', (req, res) => {
+//     res.send({hi: 'Welcome'});
+// })
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
